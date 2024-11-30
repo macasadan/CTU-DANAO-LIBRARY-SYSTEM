116bfa8b62985d7e29c1d9d42299f97897f5708b
jehanbombio04 <jehanbombio04@gmail.com>
2024-11-30 20:48:10 +0800

    test

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -463,7 +463,7 @@
                 <div class="group">
                     <div class="relative overflow-hidden bg-white rounded-xl shadow-lg transition-all duration-300 transform hover:-translate-y-2 hover:shadow-2xl">
                         <div class="absolute inset-0 bg-gradient-to-t from-blue-900/80 to-transparent opacity-0 group-hover:opacity-100 transition-opacity duration-300"></div>
-                        <img src="{{ asset('images/staff22.png') }}" alt="Ferjan Labandero" class="w-full h-50 object-cover object-center">
+                        <img src="{{ asset('images/staff22.png') }}" alt="Ferjan B. Labandero" class="w-full h-50 object-cover object-center">
                         <div class="p-6 relative z-10">
                             <h3 class="text-2xl font-bold text-blue-800 group-hover:text-yellow-400 transition-colors duration-300">Ferjan B. Labandero</h3>
                             <p class="text-blue-600 font-medium mb-4">Library Staff</p>
