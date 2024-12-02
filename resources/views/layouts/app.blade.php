699c5170444110d75047ffb12f281053ba83a543
Dan Andrei Y. Macasa <danmacasa4@gmail.com>
2024-12-02 21:59:47 +0800

    50

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -44,7 +44,8 @@
 
                 {{-- Navigation Links --}}
                 <!-- Navigation Links -->
-                <nav class="p-4 space-y-2 flex-grow overflow-y-auto pb-20">
+                <nav class="p-4 space-y-2 flex-grow overflow-y-auto pb-50">
+
                     <a href="{{ route('dashboard') }}" class="flex items-center p-3 text-gray-700 rounded-lg hover:bg-red-50 hover:text-red-600 group">
                         <svg class="w-5 h-5 text-red-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6"></path>
@@ -116,12 +117,13 @@
                                     @csrf
                                     <button type="submit" class="text-sm text-gray-500 hover:text-red-600">Logout</button>
                                 </form>
+
                             </div>
                         </div>
                     </div>
             </div>
-            </nav>
 
+            </nav>
         </aside>
 
         {{-- Main Content --}}
