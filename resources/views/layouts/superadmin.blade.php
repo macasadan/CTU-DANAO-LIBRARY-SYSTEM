cdb8b2d4a45c24493b6dfefa9875883dbaba3205
Dan Andrei Y. Macasa <danmacasa4@gmail.com>
2024-12-02 16:44:52 +0800

    change color of burger

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -16,7 +16,7 @@
     <div class="flex min-h-screen">
         <!-- Mobile Burger Menu Button -->
         <button id="mobile-menu-toggle" class="fixed top-4 left-4 z-50 md:hidden">
-            <svg class="w-8 h-8 text-white" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+            <svg class="w-8 h-8 text-gray-600" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
             </svg>
         </button>
