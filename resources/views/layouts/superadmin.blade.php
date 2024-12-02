1a2de9303d72047f69474f72808ac5453c68777d
Dan Andrei Y. Macasa <danmacasa4@gmail.com>
2024-12-02 16:20:10 +0800

    fix again ui

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -56,7 +56,7 @@
                     Manage Admins
                 </a>
                 <a href="{{ route('super-admin.user-management') }}"
-                    class="flex items-center py-3 px-6 hover:bg-indigo-700 {{ request()->routeIs('super-admin.manage-admins') ? 'bg-indigo-700' : '' }}">
+                    class="flex items-center py-3 px-6 hover:bg-indigo-700 {{ request()->routeIs('super-admin.user-management') ? 'bg-indigo-700' : '' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                         <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
                     </svg>
