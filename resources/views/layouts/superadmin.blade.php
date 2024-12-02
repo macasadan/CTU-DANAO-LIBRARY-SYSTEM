396322d0ee3ec36585c4726e1db6214afc6be54b
Dan Andrei Y. Macasa <danmacasa4@gmail.com>
2024-12-02 16:37:26 +0800

    add icons

--- resources/views/layouts/superadmin.blade.php
+++ resources/views/layouts/superadmin.blade.php
@@ -6,6 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>@yield('title', 'Super Admin Dashboard')</title>
+    <link rel="icon" type="image/jpg" href="{{ asset('/images/logo.jpg') }}">
     @viteReactRefresh
     @vite(['resources/css/app.css', 'resources/js/app.jsx'])
     @stack('styles')
