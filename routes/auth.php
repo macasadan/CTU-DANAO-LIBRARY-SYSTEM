c1cfa49a91cbe425c7d2d1e44de623885740c9b1
taetanzkie <155893139+taetanzkie@users.noreply.github.com>
2024-12-03 22:02:54 +0800

    Update auth.php

--- routes/auth.php
+++ routes/auth.php
@@ -26,7 +26,7 @@ Route::middleware('guest')->group(function () {
         ->name('password.request');
 
     Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-        ->middleware(['signed', 'throttle:5,1'])
+        ->middleware([ 'throttle:5,1'])
         ->name('password.email');
 
     Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
